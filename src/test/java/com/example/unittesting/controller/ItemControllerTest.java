8c90243603ede62c4c1bccda68b3fdcdb07d0b14
Dev9tkov <idev9tkov@gmail.com>
2021-06-26 10:52:34 +0500

    .33
    Writing Unit Test for Buisness Layer - Mocking

--- src/test/java/com/example/unittesting/controller/ItemControllerTest.java
+++ src/test/java/com/example/unittesting/controller/ItemControllerTest.java
@@ -14,6 +14,9 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -51,6 +54,22 @@ public class ItemControllerTest {
         MvcResult result = mockMvc.perform(request)
                 .andExpect(status().isOk())
                 .andExpect(content().json("{id:2,name:Item2,price:10,quantity:10}"))
+                .andReturn();
+    }
+
+    @Test
+    public void retrieveAllItems_basic() throws Exception {
+        when(buisnessService.retreiveAllItems()).
+                thenReturn(
+                        Arrays.asList(new Item(2, "Item2", 10, 10),
+                        new Item(3, "Item3", 20, 20))
+                );
+        RequestBuilder request = MockMvcRequestBuilders
+                .get("/all-items-from-database")
+                .accept(MediaType.APPLICATION_JSON);
+        MvcResult result = mockMvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().json("[{id:2,name:Item2,price:10,quantity:10}, {id:3,name:Item3,price:20,quantity:20}]"))
                 .andReturn();
     }
 }
